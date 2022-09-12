cb88f4335bd38da690bce5b3b718bed474c9491f
Kojo Kumi <Kojokumi16.kk@gmail.com>
2022-09-12 08:29:42 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+ * main - Using the main
+ * Printf: this is used to print complied code to the screen
+ * return: 0
+ */
+
+main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art");
+	return (0);
+}
