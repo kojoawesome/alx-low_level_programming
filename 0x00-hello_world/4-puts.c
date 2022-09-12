f107f4bee4ba96c4484cd6daf6f3a4ee70f0870a
Kojo Kumi <Kojokumi16.kk@gmail.com>
2022-09-12 08:08:52 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - print the string in the put function
+ *
+ * Description: Executing main function
+ * This prints "Programming is like building a multilingual puzzle"
+ * Returns: 0
+ */
+
 int main(void)
 { 
 	puts("\"Programming is like building a multilingual puzzle"); 
