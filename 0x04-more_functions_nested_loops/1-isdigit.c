8dd57f6793a97724feafa2996fc5ba25c7b4a61b
Kojo Kumi <Kojokumi16.kk@gmail.com>
2022-09-16 20:20:56 -0700

    isdigit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,13 +1,17 @@
 #include "main.h"
 
 /**
-*mul - multiplies two numbers
-*@a: first number
-*@b: second number
-*Return: returns result
+*_isdigit - checks whether a  character is a digit or not
+*@c: tested character
+*Return: 1 if it is, 0 otherwise
 */
 
-int mul(int a, int b)
+int _isdigit(int c)
 {
-	return (a * b);
+	if ((c >= 48) && (c <= 57))
+	{
+		return (1);
+	}
+
+	return (0);
 }
