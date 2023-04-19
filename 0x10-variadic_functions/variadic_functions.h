9055e14828b0468a5d43cb3a2051a5ecacd6cd81
toshflux <tonnitizzo@gmail.com>
2023-04-19 15:48:08 +0300

    updated old files

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,15 +2,17 @@
 #define _VARIADIC_FUNCTIONS_H_
 
 #include <stdarg.h>
+
 /**
  * struct print - print type with corresponding print function
  * @t: print type
  * @f: print function
-*/
+ */
+
 typedef struct print
 {
-    char *t;
-    void (*f)(va_list);
+	char *t;
+	void (*f)(va_list);
 } print_t;
 
 int _putchar(char);
@@ -19,4 +21,4 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
-#endif /* _VARIADIC_FUNCTIONS_H_*/
+#endif /* _VARIADIC_FUNCTIONS_H_ */
